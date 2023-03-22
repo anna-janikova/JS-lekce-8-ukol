c76cf189594ab1da80ab720117c7ddb8b4fc7afa
anna-janikova <108194531+anna-janikova@users.noreply.github.com>
2023-03-22 21:11:57 +0100

    Update index.js

--- kostka-zadani/index.js
+++ kostka-zadani/index.js
@@ -2,12 +2,12 @@ console.log('funguju!');
 
 let side = 1
 
-const dice = document.getElementsByClassName("dice")
+const dice = document.querySelector(".dice")
 
 const turnDice = () => {
     side = side + 1;
     if (side === 7) { side = 1 }
-    dice.src = `img/side${turnDice}.svg`
+    dice.src = `img/side${side}.svg`
 }
 
 document.addEventListener('keydown', turnDice)
